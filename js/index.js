157ee46790e59a1222bc2145feec1901cb1bb649
RAMDhino <128151749+RAMDhino@users.noreply.github.com>
2023-03-26 17:04:39 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let parent = document.getElementById("parent"); 
 let card1 = document.getElementById("card_1");
+let card2 = document.getElementById("card_2");
 
 function toggleDark() {
   parent.classList.toggle("toggle-dark-class");
   card1.classList.toggle("bg-dark");
+  card2.classList.toggle("bg-dark");
 }
